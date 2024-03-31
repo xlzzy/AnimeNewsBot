bb3982301ef36409198387eb8df9816a32d13633
xlzzy <141939403+xlzzy@users.noreply.github.com>
2024-03-31 17:56:31 +0530

    Create anime.js

---
+++ commands/anime.js
@@ -0,0 +1,11 @@
+// anime.js
+module.exports = {
+  // Define functions for anime-related commands
+  searchAnime: function (query) {
+    // Function to search for anime titles
+  },
+  getAnimeInfo: function (title) {
+    // Function to retrieve information about a specific anime
+  },
+  // Add more functions as needed
+};
