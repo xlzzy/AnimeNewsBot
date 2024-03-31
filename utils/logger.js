e3739531a7b6e8f8cbdd2519d27edb74a6fe40cd
xlzzy <141939403+xlzzy@users.noreply.github.com>
2024-03-31 17:58:27 +0530

    Create logger.js

---
+++ utils/logger.js
@@ -0,0 +1,11 @@
+// logger.js
+module.exports = {
+  // Define functions for logging messages, errors, etc.
+  logMessage: function (message) {
+    // Function to log messages
+  },
+  logError: function (error) {
+    // Function to log errors
+  },
+  // Add more functions as needed
+};
