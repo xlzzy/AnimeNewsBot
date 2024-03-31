cb47e27cc2ceea5f662cdd1c7a8bb02e6a33514e
xlzzy <141939403+xlzzy@users.noreply.github.com>
2024-03-31 17:56:50 +0530

    Create help.js

---
+++ commands/help.js
@@ -0,0 +1,7 @@
+// help.js
+module.exports = {
+  // Define a function to display help message
+  showHelp: function () {
+    // Function to display list of available commands and their descriptions
+  },
+};
